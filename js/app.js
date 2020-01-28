949dfeb6dbf2680d6cfffd84214306b255ab905d
phirom.prom <phirom.prom@PNC.LAN>
2020-01-28 17:03:22 +0700

    todo update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const dateElement = document.querySelector('#date');
-const options = {
+
+const options = {                               
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -10,23 +11,45 @@ dateElement.innerHTML = new Date().toLocaleDateString('en-US', options);
 const inputElement = document.querySelector('#input');
 const ListElement = document.querySelector('#list');
 
+const check = "fa-check-circle";
+const uncheck = "fa-circle-thin";
+const line = "lineThrough";
+let List = [];
+let id = 0;
+let data  = localStorage.getItem("todo");
+let todoList = JSON.parse(data);
+
+if(todoList){
+    todoList.forEach(item => {
+        todo(item.name, item.id, item.done, item.trash);
+    });
+}
+
 document.addEventListener('keydown', function (event) {
     if (event.key == "Enter") {
         const toDo = inputElement.value;
         const isNotEmpty = inputElement.value != "";
         if (isNotEmpty) {
            todo(toDo);
+           List.push({
+               name:toDo,
+               id:id,
+               done:false,
+               trash:false,
+            });
+            id++;
+          localStorage.setItem('todo',JSON.stringify(List));
         }
         inputElement.value = "";
     }
 });
-function todo(todo){
+function todo(todo,id,isDone,trash){
+    const isCheck = isDone? check : uncheck;
+    const isline = isDone ? line : "";
     const todos = `<li class="item">
-                <i class = "fa fa-circle-thin co"></i>
-                <p class="text">${todo}</p>
+                <i class = "fa fa-check-circle ${isCheck} co" job="complete"></i>
+                <p class="text ${isline}">${todo}</p>
                 <i class= "fa fa-trash-o de" job="delete"></i>
                 </li>`;
     ListElement.insertAdjacentHTML("beforeend", todos);
 }
-localStorage.setItem('firstname', 'phirom');
-console.log(localStorage.getItem('firstname'));
